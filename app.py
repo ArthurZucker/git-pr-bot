0ad49475ce8f8ce02b8f714d8a7c2a7629914447
Arthur Zucker <arthur.zucker@gmail.com>
2023-01-08 17:10:31 +0100

    update

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ def main():
     g = Github(os.environ["GITHUB_TOKEN"])
     repo = g.get_repo("huggingface/accelerate")
     ref = os.environ["GITHUB_REF"]
+    print(ref)
     pr_num = int(ref.split("/")[-1])
     pr = repo.get_pull(pr_num)
     pr.create_comment("### FILL ME OUT ###", path = "README.md", position = 1)
